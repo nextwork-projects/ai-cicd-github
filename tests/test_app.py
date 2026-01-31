3a9b3b8c641a29c059ff73ff3b9bb6ecbfcfa562
jeetsondbritto <168982992+jeetsondbritto@users.noreply.github.com>
2026-01-31 15:47:04 -0500

    Break a test

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,7 +10,8 @@ class TestMath:
         assert add(2, 3) == 5
     
     def test_multiply(self):
-        assert multiply(9, 9) == 81
+        assert multiply(9, 9) == 18
+    
 
     def test_add_negative(self):
         assert add(-1, -1) == -2
